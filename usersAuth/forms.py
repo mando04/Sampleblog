8acdd0cfb5a7a8f73333735ae1235da5341b86db
Armando <a.soto04@gmail.com>
2012-09-03 21:33:26 -0500

    Signed-off-by: Armando <a.soto04@gmail.com>

--- usersAuth/forms.py
+++ usersAuth/forms.py
@@ -6,7 +6,7 @@ from usersAuth.models import userAccount
 class RegisterForm(ModelForm):
     username = forms.CharField(label=(u'User Name'))
     bday = forms.DateField(label=(u'Birhday'))
-    email = forms.EmailField(label=(u'Emaill address'))
+    email = forms.EmailField(label=(u'Email address'))
     password = forms.CharField(label=(u'Password'), widget=forms.PasswordInput(render_value=False))
     password1 = forms.CharField(label=(u'Password'), widget=forms.PasswordInput(render_value=False))
  
@@ -23,12 +23,10 @@ class RegisterForm(ModelForm):
             return username
         raise forms.ValidationError("That username is already taken, please try again")
 
-    def clean_password(self):
-        password = self.cleaned_data['password']
-        password1 = self.cleaned_data['password1']
-        if password != password1:
+    def clean(self):
+        if self.cleaned_data['password'] != self.cleaned_data['password1']:
             raise forms.ValidationError("The Passwords didn't match, please try again")
-        return password
+        return self.cleaned_data
 
 class loginUserForm(forms.Form):
     username = forms.CharField(label=(u'Username'))
--- usersAuth/views.py
+++ usersAuth/views.py
@@ -2,24 +2,27 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from usersAuth.forms import RegisterForm, loginUserForm
-from usersAuth.models import userAccount
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
 def usersRegister(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect('/')
-    if request.method == ['POST']:
+    if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            user = User.objects.create_user(username=form.cleaned_data['username'], email=form.cleaned_data['email'], password=form.cleaned_data['password'])
+            user = User.objects.create_user(username=form.cleaned_data['username'], \
+                                            email=form.cleaned_data['email'], \
+                                            password=form.cleaned_data['password'])
             user.save()
             USER = user.get_profile()
             USER.name = form.cleaned_data['name']
             USER.bday = form.cleaned_data['bday']
+            USER.email = form.cleaned_data['email']
             USER.save()
             return HttpResponseRedirect('/loggedin')
-        
-            
+        else:
+            return render_to_response('register.html', { 'form':form }, context_instance=RequestContext(request))    
     else:
         """ user is not submitting a form Show blank form"""
         form = RegisterForm()
@@ -50,7 +53,7 @@ def loginUser(request):
             context.update({'error':error})
             # return an 'invalid login' error page
             return render_to_response('login.html', context, context_instance=RequestContext(request))
-        HttpreponseRedirect('/')
+        HttpResponseRedirect('/')
 
     if request.user.is_authenticated():
         return HttpResponseRedirect('/')
